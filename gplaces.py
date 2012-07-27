0b40f226b25fbeda9ff2687091b53499eec785c3
Sujeet <opencognitions@gmail.com>
2012-07-26 19:21:28 -0700

    adding types parameter

--- gplaces.py
+++ gplaces.py
@@ -12,12 +12,15 @@ class gPlaces(object):
         self.baseurlbyrank = 'https://maps.googleapis.com/maps/api/place/search/{0}?key={1}&location={2},{3}&rankby={4}&sensor={5}'
         
         
-    def getPlaces(self, latitude, longitude, radius="500", rankby="distance", sensor="false", outputformat="json"):
-        targeturl = self.baseurlbyradius.format(outputformat, self.apiKey, latitude, longitude, radius, sensor)
-        return self._queryurl(targeturl)
-    
-    def getPlacesbyrank(self, latitude, longitude, radius="500", rankby="distance", sensor="false", outputformat="json"):
-        targeturl = self.baseurlbyradius.format(outputformat, self.apiKey, latitude, longitude, rankby, sensor)
+    def getPlaces(self, latitude, longitude, types=[], radius= 5*1600, rankby="", sensor="false", outputformat="json"):
+        if(len(rankby) > 0):
+            targeturl = self.baseurlbyradius.format(outputformat, self.apiKey, latitude, longitude, rankby, sensor)
+        else:
+            targeturl = self.baseurlbyradius.format(outputformat, self.apiKey, latitude, longitude, radius, sensor)
+        
+        if(len(types) > 0):
+            targeturl = targeturl + '&types='  + '|'.join(types)
+        print targeturl
         return self._queryurl(targeturl)
     
     def _queryurl(self, url):
