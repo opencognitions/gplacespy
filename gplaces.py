6f72419c222e2ce44166df133194aada044de1d2
Sujeet <opencognitions@gmail.com>
2012-07-29 10:53:16 -0700

    use url encode

--- gplaces.py
+++ gplaces.py
@@ -1,4 +1,5 @@
 import urllib2
+import urllib
 import simplejson
 
 class gPlaces(object):
@@ -8,26 +9,41 @@ class gPlaces(object):
     #AIzaSyBwX8lyMQntv4B_Mag-o7Cmn7-FXqXU_QU
     def __init__(self, apiKey):
         self.apiKey = apiKey
-        self.baseurlbyradius = 'https://maps.googleapis.com/maps/api/place/search/{0}?key={1}&location={2},{3}&radius={4}&sensor={5}'
-        self.baseurlbyrank = 'https://maps.googleapis.com/maps/api/place/search/{0}?key={1}&location={2},{3}&rankby={4}&sensor={5}'
+        self.placesurl = 'https://maps.googleapis.com/maps/api/place/search/json?{0}'
         
+    def find(self, latitude, longitude, types=[], name="", radius= 5*1600, rankby="", sensor="false"):
+        paramdict = {}
+        paramdict['key'] = self.apiKey
+        paramdict['location'] = str(latitude) + ',' + str(longitude)
         
-    def getPlaces(self, latitude, longitude, types=[], radius= 5*1600, rankby="", sensor="false", outputformat="json"):
         if(len(rankby) > 0):
-            targeturl = self.baseurlbyradius.format(outputformat, self.apiKey, latitude, longitude, rankby, sensor)
+            paramdict['rankby'] = rankby
         else:
-            targeturl = self.baseurlbyradius.format(outputformat, self.apiKey, latitude, longitude, radius, sensor)
-        
+            paramdict['radius'] = radius
+            
         if(len(types) > 0):
-            targeturl = targeturl + '&types='  + '|'.join(types)
-        print targeturl
-        return self._queryurl(targeturl)
-    
-    def _queryurl(self, url):
+            paramdict["types"] ='|'.join(types)
+        
+        if(len(name) > 0):
+            paramdict["name"] = name
+        
+        paramdict['sensor'] = sensor
+        
+        return self._queryurl(paramdict)
+        
+    def details(self, reference, sensor="false"):
+        paramdict = {}
+        paramdict['key'] = self.apiKey
+        paramdict['reference'] = reference
+        paramdict['sensor'] = sensor
+        
+        return self._queryurl(paramdict)
+        
+        
+    def _queryurl(self, paramdict):
+        url = targeturl = self.placesurl.format(urllib.urlencode(paramdict))
         print "Querying: {0}".format(url)
         results = urllib2.urlopen(url).read();
-        return simplejson.loads(results)
-        
-        
+        return simplejson.loads(results)    
 
 
